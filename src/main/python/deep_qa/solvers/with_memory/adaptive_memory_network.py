c29bbefc3619c038b885f1bef7799753f313c80a
Mark Neumann <markn@allenai.org>
2016-10-26 15:06:08 -0700

    first working Adaptive Memory Network

--- src/main/python/deep_qa/solvers/with_memory/adaptive_memory_network.py
+++ src/main/python/deep_qa/solvers/with_memory/adaptive_memory_network.py
@@ -5,61 +5,84 @@ from overrides import overrides
 import tensorflow as tf
 
 from keras import backend as K
+from keras.layers import Reshape, Layer
+from keras import initializations
+from keras.engine import InputSpec
 
 from .memory_network import MemoryNetworkSolver
 
 
-class AdaptiveMemoryNetworkSolver(MemoryNetworkSolver):
+class AdapativeStepLayer(Layer):
 
-    def __init__(self, params: Dict[str, Any]):
+    def __init__(self, one_minus_epsilon, max_computation, memory_step,
+                 initialization='glorot_uniform', name='adaptive_layer', **kwargs):
+        self.one_minus_epsilon = one_minus_epsilon
+        self.max_computation = max_computation
+        self.memory_step = memory_step
+        self.init = initializations.get(initialization)
+        self.name = name
+        super(AdapativeStepLayer, self).__init__(**kwargs)
 
-        self.one_minus_epsilon = K.variable(1.0 - params.pop("epsilon", 0.01))
-        self.max_computation = K.variable(params.pop("max_computation", 10))
+    def build(self, input_shape):
 
-        super(AdaptiveMemoryNetworkSolver, self).__init__(params)
+        self.input_spec = [InputSpec(shape=input_shape[1])]
+        input_dim = input_shape[1][1]
+        self.halting_weight = self.init((input_dim, 1), name='{}_halting'.format(self.name))
+        self.trainable_weights = [self.halting_weight]
 
-    @overrides
-    def _get_memory_network_recurrence(self):
+    def call(self, x, mask=None):
 
-        def adaptive_recurrence(encoded_question, current_memory, encoded_knowledge):
+        encoded_question, current_memory, encoded_knowledge = x
 
-            batch_mask = tf.squeeze(tf.cast(tf.ones_like(current_memory[:, 0], name= 'batch_mask'), tf.bool))
-            hop_counter = tf.squeeze(tf.zeros_like(current_memory[:, 0], name='hop_counter'))
-            halting_accumulator = tf.squeeze(tf.zeros_like(current_memory[:, 0], name='halting_accumulator'))
-            halting_accumulator_for_comparison = tf.squeeze(tf.zeros_like(current_memory[:, 0], name='halting_acc_for_comparision'))
-            memory_accumulator = tf.zeros_like(current_memory, name='memory_accumulator')
-            attended_knowledge_loop_placeholder = tf.zeros_like(current_memory, name='attended_knowledge_placeholder')
+        batch_mask = tf.squeeze(tf.cast(tf.ones_like(current_memory[:, 0], name= 'batch_mask'), tf.bool))
+        hop_counter = tf.squeeze(tf.zeros_like(current_memory[:, 0], name='hop_counter'))
+        halting_accumulator = tf.squeeze(tf.zeros_like(current_memory[:, 0], name='halting_accumulator'))
+        halting_accumulator_for_comparison = tf.squeeze(tf.zeros_like(current_memory[:, 0], name='halting_acc_for_comparision'))
+        memory_accumulator = tf.zeros_like(current_memory, name='memory_accumulator')
+        attended_knowledge_loop_placeholder = tf.zeros_like(current_memory, name='attended_knowledge_placeholder')
 
-            def halting_condition(batch_mask,
-                                  halting_accumulator,
-                                  halting_accumulator_for_comparison,
-                                  hop_counter,
-                                  encoded_question,
-                                  current_memory,
-                                  encoded_knowledge,
-                                  memory_accumulator,
-                                  attended_knowledge_placeholder):
-                probability_condition = tf.less(halting_accumulator_for_comparison, self.one_minus_epsilon)
-                max_computation_condition = tf.less(hop_counter, self.max_computation)
-                combined_conditions = tf.logical_and(probability_condition, max_computation_condition)
-                return tf.reduce_any(combined_conditions)
+        def halting_condition(batch_mask,
+                              halting_accumulator,
+                              halting_accumulator_for_comparison,
+                              hop_counter,
+                              encoded_question,
+                              current_memory,
+                              encoded_knowledge,
+                              memory_accumulator,
+                              attended_knowledge_placeholder):
 
-            _, _, _, hop_counter, _, _, _, current_memory, attended_knowledge =\
-                tf.while_loop(cond=halting_condition, body=self.adaptive_memory_hop,
-                              loop_vars=[
-                                  batch_mask,
-                                  halting_accumulator,
-                                  halting_accumulator_for_comparison,
-                                  hop_counter,
-                                  encoded_question,
-                                  current_memory,
-                                  encoded_knowledge,
-                                  memory_accumulator,
-                                  attended_knowledge_loop_placeholder
-                              ])
-            return current_memory, attended_knowledge
+            probability_condition = tf.less(halting_accumulator_for_comparison, self.one_minus_epsilon)
+            max_computation_condition = tf.less(hop_counter, self.max_computation)
 
-        return adaptive_recurrence
+            combined_conditions = tf.logical_and(probability_condition, max_computation_condition)
+
+            return tf.reduce_any(combined_conditions)
+
+        _, _, _, hop_counter, _, _, _, current_memory, attended_knowledge =\
+            tf.while_loop(cond=halting_condition, body=self.adaptive_memory_hop,
+                          loop_vars=[
+                              batch_mask,
+                              halting_accumulator,
+                              halting_accumulator_for_comparison,
+                              hop_counter,
+                              encoded_question,
+                              current_memory,
+                              encoded_knowledge,
+                              memory_accumulator,
+                              attended_knowledge_loop_placeholder
+                          ])
+
+        current_memory = Reshape((5,))(current_memory)
+        attended_knowledge = Reshape((5,))(attended_knowledge)
+        print(current_memory, attended_knowledge)
+
+        return [current_memory, attended_knowledge]
+
+    def compute_mask(self, input, input_mask=None):
+        return [None, None]
+
+    def get_output_shape_for(self, input_shapes):
+        return [(input_shapes[1][0], input_shapes[1][1]), (input_shapes[1][0], input_shapes[1][1])]
 
     def adaptive_memory_hop(self, batch_mask,
                             halting_accumulator,
@@ -74,7 +97,7 @@ class AdaptiveMemoryNetworkSolver(MemoryNetworkSolver):
         current_memory, attended_knowledge = self.memory_step(encoded_question, previous_memory, encoded_knowledge)
 
         with tf.variable_scope("halting_calculation"):
-            halting_probability = tf.squeeze(tf.sigmoid(tf.nn.rnn_cell._linear(current_memory, 1, bias=True)))
+            halting_probability = tf.squeeze(tf.sigmoid(K.dot(current_memory, self.halting_weight)))
 
         print("halting prob:", K.shape(halting_probability))
         new_batch_mask = tf.logical_and(
@@ -82,7 +105,7 @@ class AdaptiveMemoryNetworkSolver(MemoryNetworkSolver):
                     self.one_minus_epsilon), batch_mask)
         new_float_mask = tf.cast(new_batch_mask, tf.float32)
         halting_accumulator += halting_probability * new_float_mask
-        halting_accumulator_for_comparison += halting_probability * tf.cast(batch_mask, tf.float32)
+        halting_accumulator_for_comparison += halting_probability * tf.expand_dims(tf.cast(batch_mask, tf.float32), 1)
 
         def use_probability():
             masked_halting_probability = tf.tile(tf.expand_dims(halting_probability * new_float_mask, 1), [1, 5])
@@ -118,6 +141,27 @@ class AdaptiveMemoryNetworkSolver(MemoryNetworkSolver):
             print(elt)
 
         return return_elts
+
+class AdaptiveMemoryNetworkSolver(MemoryNetworkSolver):
+
+    def __init__(self, params: Dict[str, Any]):
+
+        self.one_minus_epsilon = K.variable(1.0 - params.pop("epsilon", 0.01))
+        self.max_computation = K.variable(params.pop("max_computation", 10))
+        super(AdaptiveMemoryNetworkSolver, self).__init__(params)
+
+    @overrides
+    def _get_memory_network_recurrence(self):
+
+        def adaptive_recurrence(encoded_question, current_memory, encoded_knowledge):
+            adaptive_layer = AdapativeStepLayer(self.one_minus_epsilon, self.max_computation, self.memory_step)
+            return adaptive_layer([encoded_question, current_memory, encoded_knowledge])
+
+        return adaptive_recurrence
+
+
+
+
 
 
 
