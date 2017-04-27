3b0c736007e3ebb7fb3a666cd3db8e22e75de5ba
Mark Neumann <markn@allenai.org>
2017-04-27 11:11:43 -0700

    adding tensorflow device placement logging

--- deep_qa/run.py
+++ deep_qa/run.py
@@ -2,6 +2,7 @@ from typing import Union, List
 import sys
 import logging
 import shutil
+import os
 
 import random
 import pyhocon
@@ -71,6 +72,7 @@ def run_model(param_path: str, model_class=None):
     # These have to be imported _after_ we set the random seed,
     # because keras uses the numpy random seed.
     from deep_qa.models import concrete_models
+    import tensorflow
     from keras import backend as K
 
     log_dir = params.get("model_serialization_prefix", None)  # pylint: disable=no-member
@@ -82,6 +84,17 @@ def run_model(param_path: str, model_class=None):
         handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - %(message)s'))
         logging.getLogger().addHandler(handler)
         shutil.copyfile(param_path, log_dir + "_model_params.json")
+
+
+    num_thread = int(os.environ.get('OMP_NUM_THREADS'))
+    config = {
+        "allow_soft_placement": True,
+        "log_device_placement": True
+    }
+    if num_thread is not None:
+        config["intra_op_parallelism_threads"] = num_thread
+    global_session = tensorflow.Session(tensorflow.ConfigProto(**config))
+    K.set_session(global_session)
 
     if model_class is None:
         model_type = params.pop_choice('model_class', concrete_models.keys())
--- deep_qa/training/multi_gpu.py
+++ deep_qa/training/multi_gpu.py
@@ -1,11 +1,19 @@
 from keras.layers import merge
 from keras.layers.core import Lambda
-from keras.models import Model
-
+from .models import DeepQaModel
 import tensorflow as tf
 
 
-def make_parallel(model, gpu_count):
+def make_parallel(model: DeepQaModel, gpu_count: int):
+
+    """
+    :param model: An instance of a DeepQaModel.
+    :param gpu_count: The number of GPUs to duplicate the model across.
+    :return:
+    """
+
+    # Argument to a Lambda layer which will slice our large batches
+    # along the batch dimension and return a given slice.
     def get_slice(data, idx, parts):
         shape = tf.shape(data)
         size = tf.concat([shape[:1] // parts, shape[1:]], axis=0)
@@ -17,16 +25,21 @@ def make_parallel(model, gpu_count):
     for i in range(len(model.outputs)):
         outputs_all.append([])
 
-    # Place a copy of the model on each GPU, each getting a slice of the batch
+    # Place a copy of the model on each GPU, each getting a slice of the batch.
     for i in range(gpu_count):
         with tf.device('/gpu:%d' % i):
             with tf.name_scope('tower_%d' % i) as scope:
 
                 inputs = []
-                # Slice each input into a piece for processing on this GPU
-                for x in model.inputs:
-                    input_shape = tuple(x.get_shape().as_list())[1:]
-                    slice_n = Lambda(get_slice, output_shape=input_shape, arguments={'idx': i, 'parts': gpu_count})(x)
+                # Slice each input into a piece for processing on this GPU.
+                for model_input in model.inputs:
+                    # Get the shape of everything apart from the batch,
+                    # which will be split across the GPUs.
+                    output_shape = tuple(model_input.get_shape().as_list())[1:]
+                    slice_layer = Lambda(get_slice,
+                                         output_shape=output_shape,
+                                         arguments={'idx': i, 'parts': gpu_count})
+                    slice_n = slice_layer(model_input)
                     inputs.append(slice_n)
 
                 outputs = model(inputs)
@@ -35,8 +48,8 @@ def make_parallel(model, gpu_count):
                     outputs = [outputs]
 
                 # Save all the outputs for merging back together later
-                for l in range(len(outputs)):
-                    outputs_all[l].append(outputs[l])
+                for output_index in range(len(outputs)):
+                    outputs_all[output_index].append(outputs[output_index])
 
     # merge outputs on CPU
     with tf.device('/cpu:0'):
@@ -44,4 +57,4 @@ def make_parallel(model, gpu_count):
         for outputs in outputs_all:
             merged.append(merge(outputs, mode='concat', concat_axis=0))
 
-        return Model(input=model.inputs, output=merged)
+        return DeepQaModel(input=model.inputs, output=merged)
