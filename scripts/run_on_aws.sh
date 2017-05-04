53722d67edc05440b2791ecf975811fa63395050
Mark Neumann <markn@allenai.org>
2017-05-04 11:03:34 -0700

    check if repository exists before creating it

--- scripts/run_on_aws.sh
+++ scripts/run_on_aws.sh
@@ -18,11 +18,17 @@ eval $(aws --region=us-west-2 ecr get-login)
 
 docker pull 896129387501.dkr.ecr.us-west-2.amazonaws.com/infrastructure/aristo/cuda:8
 
-# Create container - we can't push to a conatiner which doesn't exist, unlike bintray.
-aws --region=us-west-2 ecr create-repository --repository-name $CONTAINER_NAME
+# Create container - we can't push to a conatiner which doesn't exist,
+# unlike bintray, but we also can't create a container which does exist,
+# so we have to check if it is there first.
+if [aws --region=us-west-2 ecr describe-repositories | grep -o "\"repositoryName\": *\"$CONTAINER_NAME\""] ; then
+    echo "Repository exists - building image."
+else
+    echo "Creating repository:"
+    aws --region=us-west-2 ecr create-repository --repository-name $CONTAINER_NAME
+fi
 
-docker build -t $CONTAINER_NAME . --build-arg PARAM_FILE=$PARAM_FILE
-
+docker build -t 896129387501.dkr.ecr.us-west-2.amazonaws.com/$CONTAINER_NAME . --build-arg PARAM_FILE=$PARAM_FILE
 docker push 896129387501.dkr.ecr.us-west-2.amazonaws.com/$CONTAINER_NAME
 
 $ARISTO_BINARY runonce --gpu 896129387501.dkr.ecr.us-west-2.amazonaws.com/$CONTAINER_NAME
