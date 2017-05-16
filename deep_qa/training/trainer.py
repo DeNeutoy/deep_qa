a7d20a03423cec7d734e3c647dc02a53d421a746
Mark Neumann <markn@allenai.org>
2017-05-16 12:37:43 -0700

    correctly get number of lambda layers

--- deep_qa/training/trainer.py
+++ deep_qa/training/trainer.py
@@ -607,7 +607,8 @@ class Trainer:
         the vocabulary of your model, you can save it here. The model config is saved by default.
         """
         if self.num_gpus > 1:
-            model_config = self.model.layers[-(self.num_gpus + 1)].to_json()
+            lambda_layers = len(self.model.outputs)
+            model_config = self.model.layers[-(lambda_layers + 1)].to_json()
         else:
             model_config = self.model.to_json()
         model_config_file = open("%s_config.json" % (self.model_prefix), "w")
