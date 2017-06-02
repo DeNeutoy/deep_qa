11dda8b5f1f46b83b434a58db310e43d9ee78e7e
Mark Neumann <markn@allenai.org>
2017-06-02 16:03:22 -0700

    refactor gradient computation functions

--- deep_qa/training/train_utils.py
+++ deep_qa/training/train_utils.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
+from collections import defaultdict
 import tensorflow
 
 
@@ -19,85 +20,82 @@ def pin_variable_device_scope(device, variable_device="/cpu:0"):
     return _assign
 
 
-def average_gradients(tower_gradients: List[List[Tuple]]):
+def average_gradients(tower_gradients: List[List[Tuple[List[tensorflow.Tensor], tensorflow.Tensor]]]):
     """
     Given a list of (gradient, variable) pairs from the result of
     a gradient calculation from multiple GPUs, calculate their
     average.
     """
     # Make a map from variables -> [gradients that are not none].
-    gradient_map = {}
+    gradient_map = defaultdict(list)
     for tower in tower_gradients:
         for grad, variable in tower:
             if grad is not None:
-                if variable not in gradient_map:
-                    gradient_map[variable] = []
                 gradient_map[variable].append(grad)
-    average_gradient_list = []
 
+    average_gradient_list = []
     for variable, gradients in gradient_map.items():
         # variable is a tensor.
         # gradients is a list of gradients for this tensor to average.
-
         # Pick any one of the gradients to see if it is an IndexedSlice.
-
         first_actual_grad = gradients[0]
         if isinstance(first_actual_grad, tensorflow.IndexedSlices):
-            # If the gradient is type IndexedSlices then this is a sparse
-            # gradient with attributes indices and values. To average, we
-            # need to concat them individually and then create a new
-            # IndexedSlices object.
-            indices = []
-            values = []
-            for grad in gradients:
-                indices.append(grad.indices)
-                values.append(grad.values)
-            all_indices = tensorflow.concat(indices, 0)
-            avg_values = tensorflow.concat(values, 0) / len(gradients)
-            # Deduplicate across indices.
-            values, unique_indicies = _deduplicate_indexed_slices(avg_values, all_indices)
-            sparse_grad = tensorflow.IndexedSlices(values,
-                                                   unique_indicies,
-                                                   dense_shape=first_actual_grad.dense_shape)
-            average_gradient_list.append((sparse_grad, variable))
+            averaged_gradient = _get_sparse_gradient_average(gradients)
+            average_gradient_list.append((averaged_gradient, variable))
         else:
-            # A normal tensor can just do a simple average.
-            grads_expanded = []
-            for grad in gradients:
-                # Add a 0 dimension to the gradients to represent the tower and
-                # append on a 'tower' dimension which we will average over.
-                grads_expanded.append(tensorflow.expand_dims(grad, 0))
-
-            # Average over the 'tower' dimension.
-            grad = tensorflow.concat(grads_expanded, 0)
-            mean_grad = tensorflow.reduce_mean(grad, 0)
-            average_gradient_list.append((mean_grad, variable))
-
+            averaged_gradient = _get_dense_gradient_average(gradients)
+            average_gradient_list.append((averaged_gradient, variable))
     assert len(average_gradient_list) == len(gradient_map)
     return average_gradient_list
 
 
-def _deduplicate_indexed_slices(values, indices):
+def _get_dense_gradient_average(gradients: List[tensorflow.Tensor]):
     """
-    Sums `values` associated with any non-unique `indices`.
-
-    Parameters
-    ----------
-    values: A `Tensor` with rank >= 1.
-    indices: A one-dimensional integer `Tensor`, indexing into the first
-      dimension of `values` (as in an IndexedSlices object).
-
-    Returns
-    -------
-    A tuple of (`summed_values`, `unique_indices`) where `unique_indices` is a
-    de-duplicated version of `indices` and `summed_values` contains the sum of
-    `values` slices associated with each unique index.
+    A normal tensor can just do a simple average. Here, we stack all the gradients into a
+    tensor and then average over the dimension which they were stacked into.
     """
-    unique_indices, new_index_positions = tensorflow.unique(indices)
-    summed_values = tensorflow.unsorted_segment_sum(
-            values, new_index_positions,
+    grads_expanded = []
+    for grad in gradients:
+        # Add a 0 dimension to the gradients to represent the tower and
+        # append on a 'tower' dimension which we will average over.
+        grads_expanded.append(tensorflow.expand_dims(grad, 0))
+
+    # Average over the 'tower' dimension.
+    grad = tensorflow.concat(grads_expanded, 0)
+    mean_grad = tensorflow.reduce_mean(grad, 0)
+
+    return mean_grad
+
+
+def _get_sparse_gradient_average(gradients: List[tensorflow.IndexedSlices]):
+    """
+    If the gradient is an instance of an IndexedSlices then this is a sparse
+    gradient with attributes indices and values. To average, we
+    need to concat them individually and then create a new
+    IndexedSlices object. This case frequently occurs in the embedding layers
+    of neural network models, as for a given input, only some indices of the
+    embedding are updated, so performing sparse updates using IndexedSlices
+    is considerably more efficient.
+    """
+    indices = []
+    values = []
+    first_actual_gradient = gradients[0]
+    for grad in gradients:
+        indices.append(grad.indices)
+        values.append(grad.values)
+    all_indices = tensorflow.concat(indices, 0)
+    avg_values = tensorflow.concat(values, 0) / len(gradients)
+
+    # Deduplicate across indices.
+    unique_indices, new_index_positions = tensorflow.unique(all_indices)
+    values = tensorflow.unsorted_segment_sum(
+            avg_values, new_index_positions,
             tensorflow.shape(unique_indices)[0])
-    return summed_values, unique_indices
+
+    sparse_grad = tensorflow.IndexedSlices(values,
+                                           unique_indices,
+                                           dense_shape=first_actual_gradient.dense_shape)
+    return sparse_grad
 
 
 def slice_batch(batch, num_gpus):
