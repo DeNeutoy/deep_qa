dca9a14d58661aa9d1c3b1f1fba41bf2933219bd
Mark Neumann <markn@allenai.org>
2017-06-07 10:03:43 -0700

    remove question_answer_memory_network

--- deep_qa/models/multiple_choice_qa/__init__.py
+++ deep_qa/models/multiple_choice_qa/__init__.py
@@ -1,7 +1,6 @@
 from .decomposable_attention import MultipleTrueFalseDecomposableAttention
 from .multiple_true_false_memory_network import MultipleTrueFalseMemoryNetwork
 from .multiple_true_false_similarity import MultipleTrueFalseSimilarity
-from .question_answer_memory_network import QuestionAnswerMemoryNetwork
 from .question_answer_similarity import QuestionAnswerSimilarity
 from .tuple_entailment import MultipleChoiceTupleEntailmentModel
 from .tuple_inference import TupleInferenceModel
@@ -10,7 +9,6 @@ concrete_models = {  # pylint: disable=invalid-name
         'MultipleTrueFalseDecomposableAttention': MultipleTrueFalseDecomposableAttention,
         'MultipleTrueFalseMemoryNetwork': MultipleTrueFalseMemoryNetwork,
         'MultipleTrueFalseSimilarity': MultipleTrueFalseSimilarity,
-        'QuestionAnswerMemoryNetwork': QuestionAnswerMemoryNetwork,
         'QuestionAnswerSimilarity': QuestionAnswerSimilarity,
         'MultipleChoiceTupleEntailmentModel': MultipleChoiceTupleEntailmentModel,
         'TupleInferenceModel': TupleInferenceModel,
