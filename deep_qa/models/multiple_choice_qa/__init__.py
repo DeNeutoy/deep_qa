ce2db8a3a1f69cb5fa72a35b085818c067083a85
Mark Neumann <markn@allenai.org>
2017-06-07 16:10:21 -0700

    remove TrueFalseMemoryNetwork and TrueFalseDecomposableAttention

--- deep_qa/models/multiple_choice_qa/__init__.py
+++ deep_qa/models/multiple_choice_qa/__init__.py
@@ -1,12 +1,8 @@
-from .decomposable_attention import MultipleTrueFalseDecomposableAttention
-from .multiple_true_false_memory_network import MultipleTrueFalseMemoryNetwork
 from .question_answer_similarity import QuestionAnswerSimilarity
 from .tuple_entailment import MultipleChoiceTupleEntailmentModel
 from .tuple_inference import TupleInferenceModel
 
 concrete_models = {  # pylint: disable=invalid-name
-        'MultipleTrueFalseDecomposableAttention': MultipleTrueFalseDecomposableAttention,
-        'MultipleTrueFalseMemoryNetwork': MultipleTrueFalseMemoryNetwork,
         'QuestionAnswerSimilarity': QuestionAnswerSimilarity,
         'MultipleChoiceTupleEntailmentModel': MultipleChoiceTupleEntailmentModel,
         'TupleInferenceModel': TupleInferenceModel,
